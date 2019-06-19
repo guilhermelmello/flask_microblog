ccfe387ede362eec7e9b66e4ebfb4090a17b5f98
melloGuilherme <guilhermelmello@yahoo.com.br>
2019-06-19 17:38:41 -0300

    Update index route to return the index html page.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    template_params = dict(
+        title='Home',
+        user={'username': 'melloGuilherme'},
+        posts=[
+            {
+                'author': {'username': 'John'},
+                'body': 'Beautiful day in Portland.'
+            },
+            {
+                'author': {'username': 'Susan'},
+                'body': 'The Avengers movie was so cool!'
+            }
+        ]
+    )
+    return render_template('index.html', **template_params)
