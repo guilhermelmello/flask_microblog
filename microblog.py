eff7d0b8c5d2c3907b73831992dbe28d86c36b9d
melloGuilherme <guilhermelmello@yahoo.com.br>
2019-06-24 22:27:08 -0300

    Add context to flash shell.

--- microblog.py
+++ microblog.py
@@ -1,1 +1,14 @@
 from app import app
+from app import db
+from app.models import User
+from app.models import Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    context = {
+        'db': db,
+        'User': User,
+        'Post': Post
+    }
+    return context
