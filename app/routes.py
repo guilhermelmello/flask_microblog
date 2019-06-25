b8530397a5a2f3c6df5afb470b2be0b37e7c3ddb
melloGuilherme <guilhermelmello@yahoo.com.br>
2019-06-25 18:08:31 -0300

    Add user login logic.

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from app import app
 from app.forms import LoginForm
+from app.models import User
 from flask import render_template
 from flask import flash
 from flask import redirect
 from flask import url_for
+from flask_login import current_user
+from flask_login import login_user
 
 
 @app.route('/')
@@ -28,12 +31,17 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        msg = 'Login required for user {}, remember_me={}'
-        msg = msg.format(form.username.data, form.remember_me.data)
-        flash(msg)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid Username or Password.')
+            return redirect(url_for('login'))
+        login_user(user, remember_me=form.remember_me.data)
         return redirect(url_for('index'))
 
     template_params = dict(
