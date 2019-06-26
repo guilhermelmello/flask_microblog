82392d66a62c9fd6b5af412c90a6d00e74a20ef2
melloGuilherme <guilhermelmello@yahoo.com.br>
2019-06-25 21:12:10 -0300

    Add User Profile Page.

--- app/routes.py
+++ app/routes.py
@@ -86,3 +86,18 @@ def register():
         form=form
     )
     return render_template('register.html', **template_params)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    template_params = dict(
+        user=user,
+        posts=posts
+    )
+    return render_template('user.html', **template_params)
