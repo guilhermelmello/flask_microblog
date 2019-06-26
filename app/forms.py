8043572bf09df8bbc9d683523aa08cb833f3e3ce
melloGuilherme <guilhermelmello@yahoo.com.br>
2019-06-25 23:40:58 -0300

    Add edit profile page.

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,14 @@
 from app.models import User
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms import PasswordField
 from wtforms import BooleanField
+from wtforms import PasswordField
+from wtforms import StringField
 from wtforms import SubmitField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired
 from wtforms.validators import Email
 from wtforms.validators import EqualTo
+from wtforms.validators import Length
 from wtforms.validators import ValidationError
 
 
@@ -36,3 +38,9 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from app import db
+from app.forms import EditProfileForm
 from app.forms import LoginForm
 from app.forms import RegistrationForm
 from app.models import User
@@ -109,3 +110,24 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+
+    template_params = dict(
+        title='Edit Profile',
+        form=form
+    )
+    return render_template('edit_profile.html', **template_params)
