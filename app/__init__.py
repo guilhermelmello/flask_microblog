b7a93ae2bc383069a2a039c0875460d37da78f8d
melloGuilherme <guilhermelmello@yahoo.com.br>
2019-07-01 19:48:08 -0300

    Include file logging support.

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
 
 import logging
+import os
 
 
 app = Flask(__name__)
@@ -17,6 +19,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 if not app.debug:
+    # sends error by email
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -37,6 +40,24 @@ if not app.debug:
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
+    # log error to a file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    file_handler = RotatingFileHandler(
+        os.path.join('logs', 'microblog.log'),
+        maxBytes=10240,
+        backupCount=10
+    )
+
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(messages)s [in %(pathname)s:%(lineno)d]'
+    ))
+
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info(f"{app.config['NAME']} startup")
 
 
 from app import routes
