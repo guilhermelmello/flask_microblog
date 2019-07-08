cb1e123c930250a61a1c9a8f3ff8c36ed405f022
melloGuilherme <guilhermelmello@yahoo.com.br>
2019-07-08 10:22:58 -0300

    Fix locale function to return selected locale.

--- app/__init__.py
+++ app/__init__.py
@@ -76,7 +76,7 @@ if not app.debug:
 @babel.localeselector
 def get_locale():
     r = request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'es'
+    return r
 
 
 from app import routes
